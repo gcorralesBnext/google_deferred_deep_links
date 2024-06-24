3f62b9dd34194079f8ca1360f999b9770fbfca6c
Gabriel Isaac Corrales Escorcia <gcorrales@bnext.com>
2024-06-24 16:35:15 -0600

    REFACTOR: remove "static" in main methods

--- lib/google_deferred_deep_links.dart
+++ lib/google_deferred_deep_links.dart
@@ -4,7 +4,7 @@ import 'google_deferred_deep_links_platform_interface.dart';
 class GoogleDeferredDeepLinks {
   static final GoogleDeferredDeepLinksPlatform _platform = GoogleDeferredDeepLinksPlatform.instance;
 
-  static Future<String?> onStart() => _platform.onStart();
+  Future<String?> onStart() => _platform.onStart();
 
-  static Stream<Map<String, dynamic>?> get deferredDeepLinkStream => _platform.deferredDeepLinkStream;
+  Stream<Map<String, dynamic>?> get deferredDeepLinkStream => _platform.deferredDeepLinkStream;
 }
